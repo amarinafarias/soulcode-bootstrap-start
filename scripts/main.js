9f2127e5ecbac148adcb25f0e0cc4c878db0277b
José Almir <jr.cod.dev@gmail.com>
2021-12-15 18:04:31 -0300

    1512

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,32 @@
+
+// Detectar evento de redimensionar tela
 window.addEventListener("resize", function (e) {
-    const brWatcher = document.getElementById("breakpoint-watcher");
-    if (brWatcher) {
-      brWatcher.innerHTML = `${window.innerWidth} x ${
-        window.innerHeight
-      } <i>(${breakpoint(window.innerWidth)})</i>`;
-    }
-  });
-  
-  function breakpoint(width) {
-    if (width < 576) {
-      return "xs";
-    } else if (width < 768) {
-      return "sm";
-    } else if (width < 992) {
-      return "md";
-    } else if (width < 1200) {
-      return "lg";
-    } else if (width < 1400) {
-      return "xl";
-    } else {
-      return "xxl";
-    }
+  // Seleciona a div que será onde irá mostrar a resolução
+  // e breakpoint
+  const brWatcher = document.getElementById("breakpoint-watcher");
+
+  // Caso ele exista na página mostre a resolução
+  if (brWatcher) {
+    brWatcher.innerHTML = `${window.innerWidth} x ${
+      window.innerHeight
+    } <i>(${breakpoint(window.innerWidth)})</i>`;
   }
+});
+
+
+// Condicional que segue a tabela da documentação
+function breakpoint(width) {
+  if (width < 576) {
+    return "xs";
+  } else if (width < 768) {
+    return "sm";
+  } else if (width < 992) {
+    return "md";
+  } else if (width < 1200) {
+    return "lg";
+  } else if (width < 1400) {
+    return "xl";
+  } else {
+    return "xxl";
+  }
+}
