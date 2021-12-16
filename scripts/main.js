d3f0f4774805bf22449592855d0f786a4b5c93a5
José Almir <jr.cod.dev@gmail.com>
2021-12-15 21:53:31 -0300

    1612

--- scripts/main.js
+++ scripts/main.js
@@ -22,3 +22,43 @@ function breakpoint(width) {
     return "xxl";
   }
 }
+
+const news = [
+  {
+    titulo: "Notícia bombástica",
+    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque voluptatum nihil dignissimos odio eum?",
+    img: "./img/img1.png",
+    categoria: "esportes",
+  },
+];
+
+class NoticiasBuilder {
+  static build(categoria) {
+    let newsCards = "";
+
+    news
+      .filter((news) => news.categoria === categoria)
+      .forEach((news) => {
+        newsCards += `<div class="col-md-6 col-lg-4 col-xl-3 mt-3">
+        <div class="card">
+          <div class="card-header">
+            <img src="${news.img}" class="img-fluid"/>
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">${news.titulo}</h5>
+            <p class="card-text">${news.desc}</p>
+            <a href="#" class="btn btn-primary">Ler mais</a>
+          </div>
+        </div>
+      </div>
+    `;
+      });
+
+    return newsCards;
+  }
+}
+
+(function () {
+  const gridEsportes = document.getElementById("grid-esportes");
+  if (gridEsportes) gridEsportes.innerHTML = NoticiasBuilder.build("esportes");
+})();
